5f71746ccfa09007cdbe55dad1b96836d2cac4e8
duckiesakaminhduc <voducminh39@gmail.com>
2023-02-11 17:41:34 +0700

    cap nhat cach lam so 2

--- ARRAY/MaxconsicutiveOnes.java
+++ ARRAY/MaxconsicutiveOnes.java
@@ -1,25 +1,45 @@
 package ARRAY;
 
 public class MaxconsicutiveOnes {
+    /* cach so 1 */
+ /* 
     public int findMaxConsecutiveOnes(int[] nums) {
-        int count = 0;
-        int max = 0;
+    int count = 0;
+    int max = 0;
+    for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+    count++;
+    } else {
+    if (count > max)
+    max = count;
+    count = 0;
+    }
+    if (count > max)
+    max = count;
+    }
+    return max;
+    }
+*/
+    /* cach so 2 */
+    int count = 0;
+    int max = 0;
+
+    public int findMaxConsecutiveOnes(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                count++;
-            } else {
-                if (count > max)
-                    max = count;
+            if (nums[i] == 0) {
                 count = 0;
+            } else {
+                count++;
+                if (count > max) {
+                    max = count;
+                }
             }
-            if (count > max)
-                max = count;
         }
         return max;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 1, 0, 1, 1, 1 };
+        int[] nums = { 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 };
         MaxconsicutiveOnes m = new MaxconsicutiveOnes();
         System.out.println(m.findMaxConsecutiveOnes(nums));
     }
